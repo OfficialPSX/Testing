99c47b8253bac7f87fb4f4fd75cd23eb917e0213
OfficialPSX <143141268+OfficialPSX@users.noreply.github.com>
2023-08-28 15:25:07 +0800

    Update Loading_Screen.lua

--- Loading_Screen.lua
+++ Loading_Screen.lua
@@ -30,14 +30,14 @@ local leave = Instance.new("TextLabel");
 local UITextSizeConstraint_5 = Instance.new("UITextSizeConstraint");
 local EndSequence = Instance.new("Frame");
 if _G.ScriptName == nil and _G.FirstText == nil and _G.SecondText == nil and _G.ThirdText == nil and _G.WaitingTime == nil then
-	_G.ScriptName = "Rizz Scriptz";
+	_G.ScriptName = "Skeptical Scripts";
 	_G.FirstText = "Preparing Script...";
 	_G.SecondText = "Loading Script...";
 	_G.ThirdText = "Almost Done...";
 	_G.WaitingTime = 180;
 end
 ;
-RLoading.Name = "RizzHub";
+RLoading.Name = "Skeptical Scripts";
 RLoading.Parent = game:GetService("CoreGui");
 RLoading.IgnoreGuiInset = true;
 RMainFrame.Name = "RMainFrame";
